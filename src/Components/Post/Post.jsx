ad7ba7e8cbc33ddac93a96f4bde08e02d0d65aba
[Omar <[00023620@uca.edu.sv]>
2021-11-26 17:51:48 -0600

    Add: icons

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,10 +1,15 @@
-
+import { IoHeartOutline} from 'react-icons/io5';
+import { IoBookmarkOutline } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
 
 export const Post = (props) => {
     return (
         <div className ="p-8 rounded-xl shadow-xl border border-coolgray-400 my-6 bg-white">
             <h1 className = "lg:text-4xl text-2xl uppercase font-bold"> {props.owner}</h1>
             <img className="rounded my-2 lg:my-8" src = {props.url} alt = {`Image posted from user ${props.owner} `} />
+            <IoHeartOutline></IoHeartOutline>
+            <AiOutlineMessage></AiOutlineMessage>
+            <IoBookmarkOutline></IoBookmarkOutline>
             <p className = "font-RobotoSlab lg:text-2xl">{props.tittle}</p>
             <p className ="font-RobotoSlab font-light lg:text-lg text-xs">{props.desc}</p>
         </div>
