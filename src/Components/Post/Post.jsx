95b4667e8b2440dc2f258cf6bf97cb1ee817c073
[Omar <[00023620@uca.edu.sv]>
2021-11-26 18:28:56 -0600

    Move: icons

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -4,12 +4,14 @@ import { AiOutlineMessage } from "react-icons/ai";
 
 export const Post = (props) => {
     return (
-        <div className ="p-8 rounded-xl shadow-xl border border-coolgray-400 my-6 bg-white">
+        <div className ="p-8 rounded-xl shadow-xl border border-coolgray-400 my-6 bg-white relative">
             <h1 className = "lg:text-4xl text-2xl uppercase font-bold"> {props.owner}</h1>
             <img className="rounded my-2 lg:my-8" src = {props.url} alt = {`Image posted from user ${props.owner} `} />
-            <IoHeartOutline></IoHeartOutline>
-            <AiOutlineMessage></AiOutlineMessage>
+            <div className ="w-max right-8 mt-1  flex flex-row-reverse absolute lg:text-4xl gap-2">
             <IoBookmarkOutline></IoBookmarkOutline>
+            <AiOutlineMessage></AiOutlineMessage>
+            <IoHeartOutline></IoHeartOutline>
+            </div>
             <p className = "font-RobotoSlab lg:text-2xl">{props.tittle}</p>
             <p className ="font-RobotoSlab font-light lg:text-lg text-xs">{props.desc}</p>
         </div>
