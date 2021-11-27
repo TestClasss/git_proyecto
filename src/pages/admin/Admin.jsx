bc9d226e8a99ae2059fdc3bc4f5a19470437c08d
nicolas2321-ux <00035819@uca.edu.sv>
2021-11-27 17:07:00 -0600

    estilos boton

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -38,9 +38,9 @@ export default function Admin() {
 
             <div className="w-4/5 lg:w-1/2 flex justify-center items-center">
              <div >
-                <button onClick={createHandler}>Crear post</button>
-                <button onClick={updateHandler}>Actualizar post</button>
-                <button onClick={ownedHandler}>Ver mis posts</button>
+                <button onClick={createHandler} className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-700 py-2 px-4 text-gray-100">Crear post</button>
+                <button onClick={updateHandler} className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-700 py-2 px-4 text-gray-100">Actualizar post</button>
+                <button onClick={ownedHandler} className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-700 py-2 px-4 text-gray-100">Ver mis posts</button>
                 </div>
                
             </div>
