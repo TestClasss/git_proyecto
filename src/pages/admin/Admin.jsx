07e7bf1af8d0dc1907e824dcc8abfdc8c1fb0f1d
RivasDiego <00121520@uca.edu.sv>
2021-11-28 03:57:44 -0600

    Style to Admin Page

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,52 +1,63 @@
-import { useUserContext } from '../../context/userContext'
-import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
-
+import { useUserContext } from "../../context/userContext";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Admin() {
-    const navigate = useNavigate()
-    const { logout } = useUserContext()
-    const [post, newPost] = useState('');
+  const navigate = useNavigate();
+  const { logout } = useUserContext();
+  const [post, newPost] = useState("");
 
-    const logoutHandler = () => {
-        logout()
-        navigate("/login")
-    }
+  const logoutHandler = () => {
+    logout();
+    navigate("/login");
+  };
 
-    const createHandler = () => {
-        navigate("/Create")
+  const createHandler = () => {
+    navigate("/Create");
+  };
 
-    }
+  const updateHandler = () => {
+    navigate("/Update");
+  };
+  const ownedHandler = () => {
+    navigate("/Owned");
+  };
 
-    const updateHandler = () => {
-        navigate("/Update")
-    }
-    const ownedHandler = () => {
-        navigate("/Owned")
-    }
+  return (
+    <section className="flex flex-col justify-center items-center min-h-screen min-w-scren">
+      <div className="p-10 w-11/12 lg:w-1/2 h-max min-h-screen flex flex-col justify-around items-center bg-gradient-to-tr from-bluegray-50 to-warmgray-50 shadow-md">
+        <h2 className="text-5xl lg:text-6xl font-extrabold text-gray-800 text-center mb-4">
+          Página de Admin
+        </h2>
+        <h3 className="text-xl lg:text-4xl font-medium text-gray-700 text-center -mt-12">
+          Has ingresado a la plataforma con credenciales de administrador
+        </h3>
 
-    return (
-        <section className="flex flex-col justify-center items-center min-h-screen min-w-scren">
-            <button onClick={logoutHandler} className="fixed bottom-5 right-2 uppercase w-max w-max transition rounded border border-blue-500 duration-300 ease-in-out lg:text-lg text-sm text-extrabold bg-blue-500 hover:bg-blue-600 py-2 px-4 text-white">Log out</button>
-            <button onClick={ownedHandler} className="fixed bottom-5 right-2 uppercase w-max w-max transition rounded border border-blue-500 duration-300 ease-in-out lg:text-lg text-sm text-extrabold bg-blue-500 hover:bg-blue-600 py-2 px-4 text-white">Ver mis posts</button>
-            <button className ="fixed bottom-16 lg:bottom-20 right-2 uppercase w-max transition rounded border border-blue-500 duration-300 ease-in-out lg:text-lg text-sm text-extrabold bg-blue-500 hover:bg-blue-600 py-2 px-4 text-white" onClick={createHandler}>Crear post</button>               
-            <div className="p-10 w-11/12 lg:w-1/2 h-max min-h-screen flex flex-col items-center bg-gradient-to-tr from-bluegray-50 to-warmgray-50 shadow-md">
-                <h2 className="text-5xl lg:text-6xl font-extrabold text-gray-800 text-center">Admin page</h2>
-                <h3 className="text-lg font-medium text-gray-700 text-center">Only users with the admin role can view this page</h3>
+        <p className="text-lg lg:text-3xl font-medium text-gray-400 text-center -mt-12">
+          Selecciona uno de los botones para realizar la acción correspondiente
+        </p>
 
-                <p className="text-xl font-medium text-gray-400 text-center mt-6">If you want to see this page you have to ask permission from the administrators of the application</p>
+        <button
+          onClick={ownedHandler}
+          className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 hover:border-blue-400 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-400 py-2 px-4 hover:text-blue-700 text-gray-100"
+        >
+          Ver mis posts
+        </button>
 
-                <button onClick={logoutHandler} className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-700 py-2 px-4 text-gray-100">Log out</button>
-            </div>
+        <button
+          className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 hover:border-blue-400 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-400 py-2 px-4 hover:text-blue-700 text-gray-100"
+          onClick={createHandler}
+        >
+          Crear post
+        </button>
 
-            <div className="w-4/5 lg:w-1/2 flex justify-center items-center">
-             <div >
-               
-                </div>
-               
-            </div>
-        </section>
-    )
-
+        <button
+          onClick={logoutHandler}
+          className="mt-6 w-1/2 lg:w-1/3 transition rounded border border-blue-500 hover:border-blue-400 duration-300 ease-in-out text-lg text-extrabold uppercase bg-blue-500 hover:bg-blue-400 py-2 px-4 hover:text-blue-700 text-gray-100"
+        >
+          Cerrar Sesión
+        </button>
+      </div>
+    </section>
+  );
 }
-    
